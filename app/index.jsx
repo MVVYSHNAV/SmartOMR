9f1cde92abe9f81e1cfd91adc2d7648629f0fbcc
vyshnav <vyshnavmv15@gmail.com>
2024-05-07 17:17:18 +0530

    font,icons,[ (auth),(tabs),(search)- folder added ]

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View  className="flex-1 items-center justify-center bg-grey" >
-      <Text className="text-3xl">SmartOMR </Text>
+      <Text className="text-3xl font-pblack">SmartOMR </Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'red'}}>  go to Profile </Link>
     </View>
